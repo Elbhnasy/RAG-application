2d6175150fc14579cefbf096468e95347dea0c9f
Elbhnasy <khaledfox20201@gmail.com>
2025-04-23 17:53:58 +0200

    Refactor welcome endpoint to use dependency injection for app settings and add data upload endpoint

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from helpers import get_settings, Settings
 import os
+
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
 )
 
 @base_router.get("/")
-async def welcome():
-    app_name  = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
-    app_description = os.getenv('APP_DESCRIPTION')
-
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+    app_description = app_settings.APP_DESCRIPTION
     return {
         "message": f"Welcome to {app_name} API",
         "version": app_version,
