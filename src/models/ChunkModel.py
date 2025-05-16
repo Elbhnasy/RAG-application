3923bdf3090cce3cf4114cb2ab31698196146eb2
Elbhnasy <khaledfox20201@gmail.com>
2025-05-16 12:42:31 +0300

    Add delete_chunks_by_project_id method to ChunkModel for removing chunks by project ID

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -31,4 +31,9 @@ class ChunkModel(BaseDataModel):
             await self.collection.bulk_write(operations)
 
         return len(chunks)
-        
+    
+    async def delete_chunks_by_project_id(self, project_id : ObjectId) -> int:
+        result = await self.collection.delete_many({
+            "chunk_project_id": project_id
+        })
+        return result.deleted_count
