a88a7fd55acafb7963440f0e16312d8d5bb326f0
Elbhnasy <khaledfox20201@gmail.com>
2025-05-17 19:50:03 +0300

    Implement AssetModel and Asset schema with collection initialization and asset management methods

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from .enums.ProcessingEnum import ProcessingEnum
 from .ProjectModel import ProjectModel
 from .ChunkModel import ChunkModel
 from .BaseDataModel import BaseDataModel
+from .AssetModel import AssetModel
