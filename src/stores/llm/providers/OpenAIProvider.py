ba720b48dd9280ae5585147dd87a662445104a11
Elbhnasy <khaledfox20201@gmail.com>
2025-05-19 14:00:13 +0300

    Add OpenAIProvider class for interfacing with OpenAI API

---
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -0,0 +1,41 @@
+from ..LLMInterface import LLMInterface
+from ..LLMEnums import OpenAIEnums
+from openai import OpenAI
+import logging
+
+class OpenAIProvider(LLMInterface):
+
+    def __init__(self, api_key: str, api_url: str, 
+                default_input_max_characters: int = 1000,
+                default_generation_max_output_tokens: int = 1000,
+                default_generation_temperature: float = 0.1):
+        
+        self.api_key = api_key
+        self.api_url = api_url
+
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
+
+        self.generation_model_id = None
+
+        self.embedding_model_id = None
+        self.embedding_size = None
+
+        self.client = OpenAI(
+            api_key=self.api_key,
+            api_url=self.api_url
+        )
+
+        self.logger = logging.getLogger(__name__)
+
+    def set_generation_model(self, model_id: str):
+        """
+        Set the generation model to be used.
+        
+        :param model_id: The ID of the model to be set.
+        """
+        self.generation_model_id = model_id
+        self.logger.info(f"Generation model set to {model_id}")
+    
+    
