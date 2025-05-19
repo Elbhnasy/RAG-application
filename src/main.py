7e70bab9eb643c578278368fb8a9e06f505ee126
Elbhnasy <khaledfox20201@gmail.com>
2025-05-19 18:31:24 +0300

    Refactor lifespan function to initialize LLM clients and improve MongoDB connection handling

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers import get_settings
 import logging
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,21 +13,33 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     settings = get_settings()
     try:
-        # Initialize client and database
         app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
         app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+        logger.info("MongoDB connection established")
         
-        # Verify connection
+        llm_provider_factory = LLMProviderFactory(settings)
+
+        # generation client
+        app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+        app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+
+        # embedding client
+        app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+        app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                                embedding_size=settings.EMBEDDING_MODEL_SIZE)
         await app.mongo_conn.admin.command("ping")
         logger.info("Connected to MongoDB (DB: %s)", settings.MONGODB_DATABASE)
+        yield
     except Exception as e:
         logger.error("MongoDB connection failed: %s", e)
         raise
-    yield
-    app.mongo_conn.close()
-    logger.info("MongoDB connection closed")
+    finally:
+        app.mongo_conn.close()
+        logger.info("MongoDB connection closed")
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
+
