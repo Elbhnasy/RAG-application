51664fe46a87973e43891e41681a8647cca0d104
Elbhnasy <khaledfox20201@gmail.com>
2025-05-20 14:47:13 +0300

    Implement QdrantDBProvider class with initialization and distance method handling

---
+++ src/stores/vectordb/providers/QdrantDBProvider.py
@@ -0,0 +1,28 @@
+from qdrant_client import QdrantClient, models
+from ..VectorDBInterface import VectorDBInterface
+from ..VectorDBEnums import VectorDBEnums, DistanceMethodEnums
+from typing import List, Dict, Any
+import logging
+
+
+class QdrantDBProvider(VectorDBInterface):
+    def __init__(self, db_bath: str, distance_method: str):
+        """
+        Initialize the QdrantDBProvider.
+
+        Args:
+            db_bath (str): The path to the Qdrant database.
+            distance_method (str): The distance method to use (e.g., "cosine", "dot").
+        """
+        self.client = None
+        self.db_bath = db_bath
+        self.distance_method = None
+
+        if distance_method == DistanceMethodEnums.COSINE.value:
+            self.distance_method = models.Distance.COSINE
+        elif distance_method == DistanceMethodEnums.DOT.value:
+            self.distance_method = models.Distance.DOT
+
+        self.logger = logging.getLogger(__name__)
+
+    
