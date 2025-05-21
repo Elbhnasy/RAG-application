cedb11aa7205c3cd4fcfb692dc585a0646539683
Elbhnasy <khaledfox20201@gmail.com>
2025-05-21 17:51:18 +0300

    Fix import statement for providers and remove unused api_url in CoHereProvider instantiation

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -1,5 +1,5 @@
 from .LLMEnums import LLMEnums
-from providers import OpenAIProvider, CoHereProvider
+from .providers import OpenAIProvider, CoHereProvider
 
 class LLMProviderFactory:
     def __init__(self, config: dict):
@@ -30,7 +30,6 @@ class LLMProviderFactory:
         if provider == LLMEnums.COHERE.value:
             return CoHereProvider(
                 api_key = self.config.COHERE_API_KEY,
-                api_url = self.config.COHERE_API_URL,
                 default_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
                 default_generation_max_output_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
                 default_generation_temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
