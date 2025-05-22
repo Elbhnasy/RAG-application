78c26a3dea9b16e5e68ce3e9eb3790cb96051496
Elbhnasy <khaledfox20201@gmail.com>
2025-05-22 17:53:09 +0300

    Refactor NLPController initialization to use template_parser and add answer_rag endpoint for RAG question handling

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -49,7 +49,7 @@ async def index_project(
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
         embedding_client=request.app.embedding_client,
-        template_parser=None
+        template_parser=request.app.template_parser,
     )
 
     has_records = True
@@ -120,7 +120,7 @@ async def get_index_info(request: Request,project_id: str,):
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
         embedding_client=request.app.embedding_client,
-        template_parser=None
+        template_parser=request.app.template_parser,
     )
 
     collection_info = nlp_controller.get_vector_db_collection_info(
@@ -159,7 +159,7 @@ async def search_index(request: Request,project_id: str,search_request: SearchRe
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
         embedding_client=request.app.embedding_client,
-        template_parser=None#request.app.template_parser
+        template_parser=request.app.template_parser,
     )
 
     search_result = nlp_controller.search_vector_db_collection(
@@ -181,5 +181,43 @@ async def search_index(request: Request,project_id: str,search_request: SearchRe
             "search_result": [
                 result.dict() for result in search_result
                 ]
+        }
+    )
+
+@nlp_router.post("/index/answer/{project_id}")
+async def answer_rag(request: Request, project_id: str, search_request: SearchRequest):
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client,
+    )
+    project = await project_model.get_project_or_create_one(
+        project_id=project_id,
+        )
+    
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
+    )
+
+    answer, full_prompt, chat_history = nlp_controller.answer_rag_question(
+        project=project,
+        query=search_request.text,
+        limit=search_request.limit,
+    )
+    if not answer:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.RAG_ANSWER_ERROR.value,
+            }
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "signal": ResponseSignal.RAG_ANSWER_SUCCESS.value,
+            "answer": answer,
+            "full_prompt": full_prompt,
+            "chat_history": chat_history
         }
     )
